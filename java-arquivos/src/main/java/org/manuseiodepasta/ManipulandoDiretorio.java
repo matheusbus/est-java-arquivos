855434de7885097fb197eaa59ad6303c54705793
Matheus Buschermoehle <matheusbuschermoehle@gmail.com>
2023-09-16 17:00:36 -0300

    feat(Manipulando diretório): Manipulando diretórios com a classe File

---
+++ java-arquivos/src/main/java/org/manuseiodepasta/ManipulandoDiretorio.java
@@ -0,0 +1,34 @@
+package org.manuseiodepasta;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class ManipulandoDiretorio {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a folter path: ");
+        String strPath = scanner.nextLine();
+
+        File path = new File(strPath);
+
+        File[] folders = path.listFiles(File::isDirectory);
+        System.out.println("FOLDERS: ");
+        for(File folder : folders) {
+            System.out.println(folder);
+        }
+
+        System.out.println("FILES: ");
+        File[] files = path.listFiles(File::isFile);
+        for(File file : files) {
+            System.out.println(file);
+        }
+
+        // Criando um novo diretório:
+        boolean sucesso = new File(strPath + "\\novo-diretorio").mkdir();
+        if(sucesso) {
+            System.out.println("Diretório criado com sucesso.");
+        }
+    }
+}
